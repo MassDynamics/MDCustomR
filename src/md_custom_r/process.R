9672be7b24f35e7ddefb0c022c73c69e3e004da1
Brendan Spinks <brendanspinks@gmail.com>
2024-12-13 15:46:30 +1100

    Return output directly from function

--- src/md_custom_r/process.R
+++ src/md_custom_r/process.R
@@ -19,35 +19,10 @@ run_transform_intensities <- function(intensities, metadata, normMethod, intensi
 
   print(names(metadata))
 
-  return_object <- list(metadata = as.data.frame(metadata))
-  print(names(return_object))
-  print(class(return_object$metadata))
-  return(return_object)
-  # output <- MDCustomR::transformIntensities(intensities = intensities,
-  #                                metadata = metadata,
-  #                                featureColname = "GroupId",
-  #                                replicateColname = "replicate",
-  #                                normMethod = normMethod)
-
-
-  # print("Names source data map")
-  # print(names(SOURCE_TO_DATA_MAP))
-  #
-  # if (!(intensitySource %in% names(SOURCE_TO_DATA_MAP))) {
-  #   stop(paste0("Invalid intensity source: ", intensitySource))
-  # }
-  #
-  # data_keys <- SOURCE_TO_DATA_MAP[[intensitySource]]
-  # intensity_table_name <- data_keys[["intensity"]]
-  # metadata_table_name <- data_keys[["metadata"]]
-  #
-  # return_object <- list(intensity_table_name = output$intensity,
-  #                       metadata_table_name = output$metadata,
-  #                       Runtime_Metadata = output$runtimeMetadata)
-  # names(return_object) <- c(intensity_table_name, metadata_table_name, "Runtime_Metadata")
-  #
-  # print(" names(return_object)")
-  # print( names(return_object))
-  #
-  # return(return_object)
+  output <- MDCustomR::transformIntensities(intensities = intensities,
+                                 metadata = metadata,
+                                 featureColname = "GroupId",
+                                 replicateColname = "replicate",
+                                 normMethod = normMethod)
+  return(output)
 }
